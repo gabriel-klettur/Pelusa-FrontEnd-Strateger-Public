6138656cc642a7b6090d075431259247e05a3ce4
Gabriel Roca <blast.blackstone@gmail.com>
2024-05-31 21:37:28 +0000

    Update App.js
    Ajustes esteticos

--- strateger-react/src/App.js
+++ strateger-react/src/App.js
@@ -7,14 +7,19 @@ import CandleChart from './components/CandleChart';
 const App = () => {
   return (
     <div className="container mx-auto p-4">
-      <div className="grid grid-cols-1 gap-4">
+      <div className="grid grid-cols-2 gap-10">
+        <div className="border-4 border-gray-500 pb-40 pr-20">
+          <CandleChart width={400} height={400} />          
+        </div>
+        <div className="border-4 border-gray-500 pb-40 pr-20">
+          <CandleChart width={400} height={400} />          
+        </div>        
+      </div>
+      <div className="border-4 border-orange-500 grid grid-cols-1 gap-4">
         <div>
           <AlarmList />          
-        </div>
-        <div>
-          <CandleChart width={600} height={400} />          
-        </div>
-      </div>
+        </div>        
+      </div>      
     </div>
   );
 };
