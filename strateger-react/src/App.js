543dbbfb78430dff9436f92f26126c8381e9d3fd
Gabriel Roca <blast.blackstone@gmail.com>
2024-05-31 12:28:59 +0000

    Update App.js
    Incorporacion de el grafico de velas

--- strateger-react/src/App.js
+++ strateger-react/src/App.js
@@ -2,11 +2,19 @@
 
 import React from 'react';
 import AlarmList from './components/AlarmList';
+import CandleChart from './components/CandleChart';
 
 const App = () => {
   return (
-    <div>
-      <AlarmList />
+    <div className="container mx-auto p-4">
+      <div className="grid grid-cols-1 gap-4">
+        <div>
+          <AlarmList />          
+        </div>
+        <div>
+          <CandleChart width={600} height={400} />          
+        </div>
+      </div>
     </div>
   );
 };
