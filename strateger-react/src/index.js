3438e544ea592f6cea57c87411ac9dab62864f90
Gabriel Roca <blast.blackstone@gmail.com>
2024-05-31 13:05:31 +0000

    Update index.js
    Quite el StricMode

--- strateger-react/src/index.js
+++ strateger-react/src/index.js
@@ -1,23 +1,20 @@
 // Path: strateger-react/src/index.js
 
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Importa la nueva API
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-// Crea el root con la nueva API
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 reportWebVitals();
