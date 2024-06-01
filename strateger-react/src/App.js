642aecfa0d217ad46708ceb674513c8a7c59dd64
Gabriel Roca <blast.blackstone@gmail.com>
2024-06-01 19:23:12 +0000

    Update App.js
    Pequeña modificacion visual

--- strateger-react/src/App.js
+++ strateger-react/src/App.js
@@ -7,13 +7,10 @@ import CandleChart from './components/CandleChart';
 const App = () => {
   return (
     <div className="container mx-auto p-4">
-      <div className="grid grid-cols-2 gap-10">
-        <div className="border-4 border-blue-500 pb-40 pr-20">
+      <div className="grid grid-cols-1 gap-10">
+        <div className="border-4 border-blue-500 pb-40 pr-10">
           <CandleChart width={400} height={400} />          
-        </div>
-        <div className="border-4 border-yellow-500 pb-40 pr-20">
-          <CandleChart width={400} height={400} />          
-        </div>        
+        </div>     
       </div>
       <div className="border-4 border-orange-500 grid grid-cols-1 gap-4">
         <div>
