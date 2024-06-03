ceb11a29ba55a1e7f81931f8aea11fbc18b6c27e
Gabriel Roca <blast.blackstone@gmail.com>
2024-06-03 19:29:47 +0000

    Update App.js

--- strateger-react/src/App.js
+++ strateger-react/src/App.js
@@ -1,19 +1,24 @@
-// Path: strateger-react/src/App.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import AlarmList from './components/AlarmList';
 import CandleStickChart from './components/CandleChart/CandleChart';
 
 const App = () => {
-  const initialInterval = '1h';  // Define el intervalo inicial
+  const initialTemporalidad = '1d';  // Define el intervalo inicial como '1d'
+  const [startDate, setStartDate] = useState("2024-03-01 00:00:00");
+  const [endDate, setEndDate] = useState("2024-06-04 23:59:59");
 
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 gap-10">
         <div className="border-4 border-blue-500 pl-4 pt-4 pb-4">
           <div>
-            <h1>Highcharts Candlestick Chart with Annotations</h1>
-            <CandleStickChart initialInterval={initialInterval} />
+            <CandleStickChart 
+              initialTemporalidad={initialTemporalidad}
+              initialStartDate={startDate}
+              initialEndDate={endDate}
+              setStartDate={setStartDate}
+              setEndDate={setEndDate}
+            />
           </div>
         </div>
       </div>
