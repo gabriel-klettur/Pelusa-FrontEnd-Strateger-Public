03e192da87075b38fa640034287df6e4596437b5
Gabriel Roca <blast.blackstone@gmail.com>
2024-06-03 20:01:01 +0000

    Update App.js
    Uso de Redux para pasar las alarmas seleccionadas

--- strateger-react/src/App.js
+++ strateger-react/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import AlarmList from './components/AlarmList';
 import CandleStickChart from './components/CandleChart/CandleChart';
 
 const App = () => {
   const initialTemporalidad = '1d';  // Define el intervalo inicial como '1d'
-  const [startDate, setStartDate] = useState("2024-03-01 00:00:00");
-  const [endDate, setEndDate] = useState("2024-06-04 23:59:59");
+  const [startDate, setStartDate] = useState("2024-05-27 00:00:00");
+  const [endDate, setEndDate] = useState("2024-05-27 23:59:59");
+  const selectedAlarms = useSelector((state) => state.alarms.selectedAlarms);
 
   return (
     <div className="container mx-auto p-4">
@@ -18,6 +20,7 @@ const App = () => {
               initialEndDate={endDate}
               setStartDate={setStartDate}
               setEndDate={setEndDate}
+              selectedAlarms={selectedAlarms}
             />
           </div>
         </div>
