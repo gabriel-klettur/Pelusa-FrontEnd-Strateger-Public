147d27c4ef2142bbbf36a8c8878568ebd878c728
Gabriel Roca <blast.blackstone@gmail.com>
2024-06-03 19:24:30 +0000

    Update CandleChart.js
    - Removido el 'Navigator' de highcharts
    - Modificado el boton de 'Zoom' de highcharts
    - Modificado el start y el end date, ahora conectado a la API de nuestro backend.
    - Agregado boton de temporalidad en la parte superior del grafico.

--- strateger-react/src/components/CandleChart/CandleChart.js
+++ strateger-react/src/components/CandleChart/CandleChart.js
@@ -6,110 +6,86 @@ import { fetchData } from './fetchData';
 
 AnnotationsModule(Highcharts);
 
-const CandleStickChart = ({ initialInterval }) => {
+const CandleStickChart = ({ initialTemporalidad, initialStartDate, initialEndDate, setStartDate, setEndDate }) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [interval, setInterval] = useState(initialInterval);
+  const [interval, setInterval] = useState(initialTemporalidad);
+  const [activeInterval, setActiveInterval] = useState(initialTemporalidad);
+
+  // Initialize the dates with the props received
+  const [startDateState, setStartDateState] = useState(initialStartDate);
+  const [endDateState, setEndDateState] = useState(initialEndDate);
+  
   const chartComponentRef = useRef(null);
 
   useEffect(() => {
     const loadData = async () => {
       setLoading(true);
-      await fetchData(interval, setData, setError, setLoading);
+      await fetchData(interval, startDateState, endDateState, setData, setError, setLoading);
     };
 
     loadData();
-  }, [interval]);
+  }, [interval, startDateState, endDateState]);
 
-  useEffect(() => {
-    if (chartComponentRef.current && data.length) {
-      const chart = chartComponentRef.current.chart;
-      chart.removeAnnotation('custom-annotation'); // Remove existing annotation if any
-      chart.addAnnotation({
-        id: 'custom-annotation',
-        labels: [{
-          point: {
-            xAxis: 0,
-            yAxis: 0,
-            x: Date.UTC(2024, 4, 31, 23, 0, 0),
-            y: data.find(d => d[0] === Date.UTC(2024, 4, 31, 23, 0, 0))[4] // Close price at specific time
-          },
-          text: 'Annotation 1'
-        }],
-        labelOptions: {
-          backgroundColor: 'rgba(255,255,255,0.5)',
-          borderColor: 'gray',
-          y: 40
-        }
-      });
-
-      chart.addAnnotation({
-        id: 'custom-annotation-2',
-        labels: [{
-          point: {
-            xAxis: 0,
-            yAxis: 0,
-            x: Date.UTC(2024, 4, 31, 22, 0, 0),
-            y: data.find(d => d[0] === Date.UTC(2024, 4, 31, 22, 0, 0))[4] // Close price at specific time
-          },
-          text: 'Annotation 2'
-        }],
-        labelOptions: {
-          backgroundColor: 'rgba(255,255,255,0.5)',
-          borderColor: 'gray',
-          y: 40
-        }
-      });
-    }
-  }, [data]);
+  const handleIntervalChange = (newInterval) => {
+    console.log('Interval selected:', newInterval);
+    setActiveInterval(newInterval);
+    setInterval(newInterval);
+  };
 
   const options = {
     chart: {
       type: 'candlestick',
+    },
+    navigator: {
+      enabled: false // Disable the navigator
+    },
+    rangeSelector: {
+      selected: 4,  // Initially select the "All" option (index 4)
+      inputEnabled: true,
+      buttons: [
+        {
+          type: 'month',
+          count: 1,
+          text: '1M',
+        },
+        {
+          type: 'month',
+          count: 3,
+          text: '3M',
+        },
+        {
+          type: 'month',
+          count: 6,
+          text: '6M',
+        },
+        {
+          type: 'year',
+          count: 1,
+          text: '1Y',
+        },
+        {
+          type: 'all',
+          text: 'All',
+        },
+      ],
+      inputDateFormat: '%Y-%m-%d',
       events: {
-        load: function () {
-          const chart = this;
-          Highcharts.addEvent(chart.rangeSelector, 'buttonClick', function (event) {
-            let newInterval;
-            switch (event.button.text) {
-              case '1m':
-                newInterval = '1m';
-                break;
-              case '5m':
-                newInterval = '5m';
-                break;
-              case '15m':
-                newInterval = '15m';
-                break;
-              case '30m':
-                newInterval = '30m';
-                break;
-              case '1h':
-                newInterval = '1h';
-                break;
-              case '4h':
-                newInterval = '4h';
-                break;
-              case 'Diario':
-                newInterval = '1d';
-                break;
-              case 'Semanal':
-                newInterval = '1w';
-                break;
-              case 'Mensual':
-                newInterval = '1M';
-                break;
-              default:
-                newInterval = '1h';
-            }
-            setInterval(newInterval);
-          });
-        }
-      }
+        afterSetExtremes: function (e) {
+          const newStartDate = new Date(e.min).toISOString().slice(0, 19).replace('T', ' ');
+          const newEndDate = new Date(e.max).toISOString().slice(0, 19).replace('T', ' ');
+          console.log('New start date:', newStartDate);
+          console.log('New end date:', newEndDate);
+          setStartDate(newStartDate);
+          setEndDate(newEndDate);
+          setStartDateState(newStartDate); // Update local state
+          setEndDateState(newEndDate); // Update local state
+        },
+      },
     },
     title: {
-      text: 'Candlestick Chart with Annotations'
+      text: 'Candlestick Chart'
     },
     xAxis: {
       type: 'datetime',
@@ -125,8 +101,8 @@ const CandleStickChart = ({ initialInterval }) => {
       data: data,
       color: 'red', // Color for bearish candles
       upColor: 'green', // Color for bullish candles
-      lineColor: 'red', // Border color of the candles
-      upLineColor: 'green', // Border color of bullish candles
+      lineColor: 'red', // Border color for bearish candles
+      upLineColor: 'green', // Border color for bullish candles
       tooltip: {
         valueDecimals: 2
       },
@@ -142,71 +118,61 @@ const CandleStickChart = ({ initialInterval }) => {
         upColor: 'green'
       }
     },
-    rangeSelector: {
-      selected: 1,
-      buttons: [
-        {
-          type: 'minute',
-          count: 1,
-          text: '1m'
-        },
-        {
-          type: 'minute',
-          count: 5,
-          text: '5m'
-        },
-        {
-          type: 'minute',
-          count: 15,
-          text: '15m'
-        },
-        {
-          type: 'minute',
-          count: 30,
-          text: '30m'
-        },
-        {
-          type: 'hour',
-          count: 1,
-          text: '1h'
-        },
-        {
-          type: 'hour',
-          count: 4,
-          text: '4h'
-        },
-        {
-          type: 'day',
-          count: 1,
-          text: 'Diario'
-        },
-        {
-          type: 'week',
-          count: 1,
-          text: 'Semanal'
-        },
-        {
-          type: 'month',
-          count: 1,
-          text: 'Mensual'
-        }
-      ]
-    },
     tooltip: {
-      split: true
+      split: true // Split tooltip into multiple boxes, one for each series
     }
   };
 
+  const buttonClasses = (interval) =>
+    `py-2 px-4 rounded-lg shadow-md font-bold transition duration-300 ${
+      activeInterval === interval ? 'bg-green-500 text-white' : 'bg-blue-500 hover:bg-blue-700 text-white'
+    }`;
+
   return (
-    <div>
-      {loading && <p>Loading...</p>}
+    <div className="p-4">
+      <div className="flex flex-wrap gap-2 mb-4">
+        <button className={buttonClasses('1m')} onClick={() => handleIntervalChange('1m')}>
+          1m
+        </button>        
+        <button className={buttonClasses('5m')} onClick={() => handleIntervalChange('5m')}>
+          5m
+        </button>
+        <button className={buttonClasses('15m')} onClick={() => handleIntervalChange('15m')}>
+          15m
+        </button>
+        <button className={buttonClasses('30m')} onClick={() => handleIntervalChange('30m')}>
+          30m
+        </button>
+        <button className={buttonClasses('1h')} onClick={() => handleIntervalChange('1h')}>
+          1h
+        </button>        
+        <button className={buttonClasses('4h')} onClick={() => handleIntervalChange('4h')}>
+          4h
+        </button>        
+        <button className={buttonClasses('1d')} onClick={() => handleIntervalChange('1d')}>
+          1d
+        </button>        
+        <button className={buttonClasses('1w')} onClick={() => handleIntervalChange('1w')}>
+          1w
+        </button>
+        <button className={buttonClasses('1M')} onClick={() => handleIntervalChange('1M')}>
+          1M
+        </button>
+      </div>
+      <div className="relative">
+        {loading && (
+          <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
+            <p className="text-lg font-bold">Loading...</p>
+          </div>
+        )}
+        <HighchartsReact
+          highcharts={Highcharts}
+          constructorType={'stockChart'}
+          options={options}
+          ref={chartComponentRef}
+        />
+      </div>
       {error && <p>Error: {error.message}</p>}
-      <HighchartsReact
-        highcharts={Highcharts}
-        constructorType={'stockChart'}
-        options={options}
-        ref={chartComponentRef}
-      />
     </div>
   );
 };
