482338ec88a9ea9198299c23017c429fc1cd2dd8
Gabriel Roca <blast.blackstone@gmail.com>
2024-06-04 17:52:49 +0000

    Update Toolbar.js
    Nueva funcionalidad para seleccionar el rango de las peticiones de las velas

--- strateger-react/src/components/CandleChart/Toolbar.js
+++ strateger-react/src/components/CandleChart/Toolbar.js
@@ -1,8 +1,21 @@
-//Path: strateger-react/src/components/CandleChart/Toolbar.js
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-import React from 'react';
+/**
+ * Toolbar component for selecting chart intervals.
+ *
+ * @param {Object} props - The component props.
+ * @param {string} props.activeInterval - The currently active interval.
+ * @param {Function} props.onIntervalChange - The callback function to handle interval change.
+ * @param {Function} props.onStartDateChange - The callback function to handle start date change.
+ * @param {Function} props.onEndDateChange - The callback function to handle end date change.
+ * @returns {JSX.Element} The rendered Toolbar component.
+ */
+const Toolbar = ({ activeInterval, onIntervalChange, onStartDateChange, onEndDateChange }) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
-const Toolbar = ({ activeInterval, onIntervalChange }) => {
   const buttonClasses = (interval) =>
     `py-2 px-4 rounded-lg shadow-md font-bold transition duration-300 ${
       activeInterval === interval ? 'bg-green-500 text-white' : 'bg-blue-500 hover:bg-blue-700 text-white'
@@ -10,17 +23,55 @@ const Toolbar = ({ activeInterval, onIntervalChange }) => {
 
   const intervals = ['1m', '5m', '15m', '30m', '1h', '4h', '1d', '1w', '1M'];
 
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
+    onStartDateChange(date.toISOString().slice(0, 19).replace('T', ' '));
+  };
+
+  const handleEndDateChange = (date) => {
+    setEndDate(date);
+    onEndDateChange(date.toISOString().slice(0, 19).replace('T', ' '));
+  };
+
   return (
-    <div className="flex flex-wrap gap-2 mb-4">
-      {intervals.map(interval => (
-        <button
-          key={interval}
-          className={buttonClasses(interval)}
-          onClick={() => onIntervalChange(interval)}
-        >
-          {interval}
-        </button>
-      ))}
+    <div className='grid grid-cols-2 gap-2'>
+      <div className="flex flex-wrap gap-2 mb-4">
+        {intervals.map(interval => (
+          <button
+            key={interval}
+            className={buttonClasses(interval)}
+            onClick={() => onIntervalChange(interval)}
+          >
+            {interval}
+          </button>
+        ))}
+      </div>
+
+      <div className="grid grid-cols-9 mb-4">
+        <div className='col-span-3'></div>
+        <div className="col-span-3">
+          <div>
+            <DatePicker
+              selected={startDate}
+              onChange={handleStartDateChange}
+              showTimeSelect
+              dateFormat="yyyy-MM-dd HH:mm:ss"
+              className="py-2 px-4 rounded-lg shadow-md font-bold transition duration-300 bg-orange-500 hover:bg-orange-700 text-white"
+            />
+          </div>
+        </div>
+        <div className="col-span-3">
+          <div>
+            <DatePicker
+              selected={endDate}
+              onChange={handleEndDateChange}
+              showTimeSelect
+              dateFormat="yyyy-MM-dd HH:mm:ss"
+              className="py-2 px-4 rounded-lg shadow-md font-bold transition duration-300 bg-orange-500 hover:bg-orange-700 text-white"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
