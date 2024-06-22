4c0e8a20f4560cb134b8a5f8890da870ca14e4c2
Gabriel Roca <blast.blackstone@gmail.com>
2024-06-22 20:13:49 +0000

    Update 'TradingViewChart' component Alarm.js
    Ahora las alarmas se muestran con mucho mas coherencia

--- strateger-react/src/components/TradingViewChart/Alarms.js
+++ strateger-react/src/components/TradingViewChart/Alarms.js
@@ -2,43 +2,69 @@
 
 export const mapAlarmsToMarkers = (selectedAlarms) => {
     return selectedAlarms.map(alarm => {
-      let color, text;
+      let color, text, position, shape;
   
       switch (alarm.Order) {
         case 'order open long':
           color = 'green';
           text = 'Entry Long';
+          position = 'belowBar';
+          shape = 'arrowUp';
           break;
         case 'order close long':
-          color = 'green';
+          color = 'red';
           text = 'Close Long';
+          position = 'aboveBar';
+          shape = 'arrowDown';
           break;
         case 'order open short':
           color = 'red';
           text = 'Entry Short';
+          position = 'aboveBar';
+          shape = 'arrowDown';
           break;
         case 'order close short':
-          color = 'red';
+          color = 'green';
           text = 'Close Short';
+          position = 'aboveBar';
+          shape = 'circle';
           break;
         case 'indicator open long':
           color = 'blue';
           text = 'Indicator Open Long';
+          position = 'belowBar';
+          shape = 'arrowUp';
           break;
         case 'indicator close long':
           color = 'orange';
           text = 'Indicator Close Long';
+          position = 'aboveBar';
+          shape = 'arrowDown';
+          break;
+        case 'indicator open short':
+          color = 'orange';
+          text = 'Indicator Open Short';
+          position = 'aboveBar';
+          shape = 'square';
+          break;
+        case 'indicator close short':
+          color = 'blue';
+          text = 'Indicator Close Short';
+          position = 'aboveBar';
+          shape = 'arrowUp';
           break;
         default:
           color = 'black';
           text = '?????';
+          position = 'aboveBar';
+          shape = 'arrowDown';
       }
   
       return {
         time: Math.floor(new Date(alarm.Time_Alert).getTime() / 1000), // Convertir el tiempo al formato Unix timestamp en segundos
-        position: 'aboveBar',
+        position: position,
         color: color,
-        shape: 'arrowDown',
+        shape: shape,
         text: text
       };
     });
