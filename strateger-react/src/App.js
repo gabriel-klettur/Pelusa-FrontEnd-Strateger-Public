0ef065df3c68b7377ef95a588bc361c588cf9820
Gabriel Roca <blast.blackstone@gmail.com>
2024-07-19 02:55:08 +0000

    Update App.js

--- strateger-react/src/App.js
+++ strateger-react/src/App.js
@@ -1,31 +1,39 @@
+// App.js
 import React from 'react';
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSelectedTab, selectSelectedTab } from './slices/tabSlice';
 import Alarms from './components/Alarms/Alarms';
 import Orders from './components/Orders/Orders';
 import LightweightChart from './components/TradingViewChart/LightweightChart';
 import { StrategyList } from './components/Strategy';
-import { Diary } from './components/Diary'; // Asegúrate de que esté importado correctamente
+import { Diary } from './components/Diary';
 import { Account } from './components/Account';
 import { Position } from './components/Positions';
-import Backtesting from './components/Backtesting/Backtesting';
 import Reloj from './components/Reloj';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const selectedTab = useSelector(selectSelectedTab);
   const initialTemporalidad = '1d'; // Define el intervalo inicial como '1d'
 
   // Obtener la fecha actual y formatearla
   const currentDate = new Date();
-  const formattedCurrentDate = currentDate.toISOString().slice(0, 19).replace('T', ' ');
+  const formattedCurrentDate = currentDate.toISOString();
 
   // Calcular la fecha de inicio (1000 días antes de la fecha actual)
   const startDate = new Date();
   startDate.setDate(currentDate.getDate() - 1000);
-  const formattedStartDate = startDate.toISOString().slice(0, 19).replace('T', ' ');
+  const formattedStartDate = startDate.toISOString();
+
+  const handleTabChange = (index) => {
+    dispatch(setSelectedTab(index));
+  };
 
   return (
     <div>
       <div className="border-4 border-orange-500">
-        <TabGroup>
+        <TabGroup selectedIndex={selectedTab} onChange={handleTabChange}>
           <TabList className="flex p-1 space-x-1 bg-blue-900/20 rounded-xl">
             <Tab className={({ selected }) =>
               `w-full py-2.5 text-sm leading-5 font-medium text-blue-700 rounded-lg
@@ -76,16 +84,16 @@ const App = () => {
               Configuracion
             </Tab>
           </TabList>
-          <TabPanels className="mt-2">          
-            <div className="border-4 border-blue-500">
-              <div className="border-4 border-green-500">
-                <LightweightChart
-                  initialTemporalidad={initialTemporalidad}
-                  initialStartDate={formattedStartDate}
-                  initialEndDate={formattedCurrentDate}
-                />
-              </div>
+          <div className="border-4 border-blue-500 mt-2">
+            <div className="border-4 border-green-500">
+              <LightweightChart
+                initialTemporalidad={initialTemporalidad}
+                initialStartDate={formattedStartDate}
+                initialEndDate={formattedCurrentDate}
+              />
             </div>
+          </div>
+          <TabPanels className="mt-2">
             <TabPanel className="bg-white rounded-xl p-3">
               <Alarms />
             </TabPanel>
@@ -105,7 +113,7 @@ const App = () => {
               <Position />
             </TabPanel>
             <TabPanel className="bg-white rounded-xl p-3">
-              <Backtesting />
+              Backtesting
             </TabPanel>
             <TabPanel className="bg-white rounded-xl p-3">
               Configuracion
