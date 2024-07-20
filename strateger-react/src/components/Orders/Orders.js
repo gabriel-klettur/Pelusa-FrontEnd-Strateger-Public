e2db8b58864798d8b4e35f02afb539db411d5278
Gabriel Roca <blast.blackstone@gmail.com>
2024-07-20 18:01:27 +0000

    Update Orders.js

--- strateger-react/src/components/Orders/Orders.js
+++ strateger-react/src/components/Orders/Orders.js
@@ -4,30 +4,30 @@ import OrderList from './OrderList/OrderList';
 
 const Orders = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-xl font-bold mb-4">Order Management</h1>      
+    <div className="container mx-auto">      
       <TabGroup>
         <TabList className="flex p-1 space-x-1 bg-blue-900/20 rounded-xl">
           <Tab className={({ selected }) =>
             `w-full py-2.5 text-sm leading-5 font-medium text-blue-700 rounded-lg
             ${selected ? 'bg-white shadow' : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'}`
           }>
-            Order List
+            Orders Perp USDT-M
           </Tab>
           <Tab className={({ selected }) =>
             `w-full py-2.5 text-sm leading-5 font-medium text-blue-700 rounded-lg
             ${selected ? 'bg-white shadow' : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'}`
           }>
-            Future Component
+            Orders Perp COIN-M
           </Tab>
         </TabList>
-        <TabPanels className="mt-2">
+        <TabPanels>
           <TabPanel className="bg-white rounded-xl p-3">
             <OrderList />
           </TabPanel>
-          <TabPanel className="bg-white rounded-xl p-3">
-            {/* Aquí puedes añadir otros componentes de tablas */}
-            <div>Contenido del futuro componente</div>
+          <TabPanel className="bg-white rounded-xl p-3">            
+            <div>
+              Waiting for BINGXAPI endpoint implementation
+            </div>
           </TabPanel>
         </TabPanels>
       </TabGroup>
