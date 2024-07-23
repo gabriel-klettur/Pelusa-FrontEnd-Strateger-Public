62e45742b63e161120d1ad3f87e07c572310e0ff
Gabriel Roca <blast.blackstone@gmail.com>
2024-07-23 18:47:15 +0000

    Fix: La logica de carga de los slices debe ser analizada y corregida

--- strateger-react/src/thunks/loadSlices.js
+++ strateger-react/src/thunks/loadSlices.js
@@ -3,8 +3,8 @@
 //import { fetchOrders } from '../slices/orderSlice';
 import { fetchStrategies } from '../slices/strategySlice';
 import { fetchDiaryEntries } from '../slices/diarySlice';
-import { fetchPerpUSDTMBalance, fetchPerpCOINMBalance, fetchSpotBalance } from '../slices/accountSlice';
-import { fetchPositionsCoinM, fetchPositionsUSDTM, fetchSpotDepositRecords } from '../slices/positionSlice';
+//import { fetchPerpUSDTMBalance, fetchPerpCOINMBalance, fetchSpotBalance } from '../slices/accountSlice';
+import { fetchPositionsCoinM, fetchPositionsUSDTM } from '../slices/positionSlice';
 //import { fetchTradingViewChartData } from '../slices/tradingViewChartSlice';
 
 export const loadSlicesInOrder = () => async (dispatch) => {
@@ -16,14 +16,15 @@ export const loadSlicesInOrder = () => async (dispatch) => {
     //}));
     //await dispatch(fetchAlarms({ limit: 100, offset: 0 }));
     //await dispatch(fetchOrders({ limit: 100, offset: 0 }));
+
+    
     await dispatch(fetchStrategies({ skip: 0, limit: 10 }));
     await dispatch(fetchDiaryEntries({ skip: 0, limit: 10 }));
-    await dispatch(fetchPerpUSDTMBalance());
-    await dispatch(fetchPerpCOINMBalance());
-    await dispatch(fetchSpotBalance());
+    //await dispatch(fetchPerpUSDTMBalance()); // Dependent on tradingViewChartData
+    //await dispatch(fetchPerpCOINMBalance()); // Dependent on tradingViewChartData
+    //await dispatch(fetchSpotBalance());     // Dependent on tradingViewChartData
     await dispatch(fetchPositionsCoinM());
-    await dispatch(fetchPositionsUSDTM());
-    await dispatch(fetchSpotDepositRecords());
+    await dispatch(fetchPositionsUSDTM());    
   } catch (error) {
     console.error('Error loading slices:', error);
   }
