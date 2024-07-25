8ff81a4c88d68ebb1b14398d753606e30b029aac
Gabriel Roca <blast.blackstone@gmail.com>
2024-07-25 11:41:22 +0000

    Refactor: TotalBalanceAccounts
    - Contiene el Balance actual en USD de todas las cuentas de bingx 'spot', 'coinm' y 'usdt'

--- strateger-react/src/components/Account/Account.js
+++ strateger-react/src/components/Account/Account.js
@@ -5,14 +5,18 @@ import React from 'react';
 import AccountSummary from './AccountSummary/AccountSummary';
 import AccoountCharts from './AccountCharts/AccountCharts';
 import AccountInformation from './AccountInformation/AccountInformation';
+import TotalBalanceAccounts from './TotalBalanceAccounts';
 
 const Account = () => {  
 
   return (
-    <div className="p-4 border-4 border-blue-500 grid grid-cols-1 gap-2">
-      <AccoountCharts />
-      <AccountSummary />
-      <AccountInformation />
+    <div>
+      <TotalBalanceAccounts />
+      <div className="p-4 border-4 border-blue-500 grid grid-cols-1 gap-2">
+        <AccoountCharts />
+        <AccountSummary />
+        <AccountInformation />
+      </div>
     </div>
   );
 };
