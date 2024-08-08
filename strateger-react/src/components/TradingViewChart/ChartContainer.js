597da9b8671c7d30d8b769b75329413980661a4e
Gabriel Roca <blast.blackstone@gmail.com>
2024-08-08 21:33:54 +0000

    style: Agregamos arte para rellenar espacios vacios en 'ChartContainer'

--- strateger-react/src/components/TradingViewChart/ChartContainer.js
+++ strateger-react/src/components/TradingViewChart/ChartContainer.js
@@ -14,11 +14,16 @@ import DiaryCalendar from '../Diary/DiaryCalendar';
 import SummaryChart from '../Account/AccountCharts/SummaryChart';
 import BacktestingForm from '../Backtesting/BacktestingForm/BacktestingForm';
 import LoadingOverlay from '../common/LoadingOverlay/LoadingOverlay';
+import alarmImage from './alarma_v1.webp';
 
 const ChartContainer = ({ initialTemporalidad, startDate, endDate, onDateChange }) => {
   const dispatch = useDispatch();
   const selectedTab = useSelector(selectSelectedTab);
-  const { data, loading, chartInterval } = useChartData(initialTemporalidad, new Date(startDate).toISOString(), new Date(endDate).toISOString());
+  const { data, loading, chartInterval } = useChartData(
+    initialTemporalidad,
+    new Date(startDate).toISOString(),
+    new Date(endDate).toISOString()
+  );
   const { chartContainerRef, candlestickSeriesRef, stochasticChartContainerRef } = useChart(data);
   useMarkers(candlestickSeriesRef, chartInterval);
 
@@ -26,20 +31,24 @@ const ChartContainer = ({ initialTemporalidad, startDate, endDate, onDateChange 
 
   const handleIntervalChange = (newInterval) => {
     setInterval(newInterval);
-    dispatch(setTradingViewChartParameters({
-      interval: newInterval,
-      startDate: new Date(startDate).toISOString(),
-      endDate: new Date(endDate).toISOString(),
-    }));
+    dispatch(
+      setTradingViewChartParameters({
+        interval: newInterval,
+        startDate: new Date(startDate).toISOString(),
+        endDate: new Date(endDate).toISOString(),
+      })
+    );
   };
 
   const handleDateChange = (newStartDate, newEndDate) => {
     onDateChange(newStartDate, newEndDate);
-    dispatch(setTradingViewChartParameters({
-      interval,
-      startDate: newStartDate.toISOString(),
-      endDate: newEndDate.toISOString(),
-    }));
+    dispatch(
+      setTradingViewChartParameters({
+        interval,
+        startDate: newStartDate.toISOString(),
+        endDate: newEndDate.toISOString(),
+      })
+    );
   };
 
   const simulatedResults = [
@@ -48,7 +57,7 @@ const ChartContainer = ({ initialTemporalidad, startDate, endDate, onDateChange 
     { date: '2024-07-03', pnl: 200 },
     { date: '2024-07-04', pnl: -30 },
     { date: '2024-07-05', pnl: 150 },
-  ];  
+  ];
 
   const renderTabContent = () => {
     switch (selectedTab) {
@@ -67,7 +76,7 @@ const ChartContainer = ({ initialTemporalidad, startDate, endDate, onDateChange 
       case 6:
         return <BacktestingForm />;
       case 7:
-        return '';      
+        return '';
       default:
         return 'Estrategias Activas';
     }
@@ -84,17 +93,24 @@ const ChartContainer = ({ initialTemporalidad, startDate, endDate, onDateChange 
         onDateChange={handleDateChange}
       />
       <div className="grid grid-cols-10 gap-1">
-        <div className="grid grid-cols-1 col-span-6 bg-white p-2 rounded-br-lg border-2 border-t border-african_violet-700">
-          <div ref={chartContainerRef} className="col-span-10 h-64 rounded-t-lg overflow-hidden border-b-2 border-african_violet-700"></div>
-          <div ref={stochasticChartContainerRef} className="col-span-10 h-32 mt-2 rounded-b-lg overflow-hidden border-t-2 border-african_violet-700"></div>
+        <div className="grid grid-cols-1 col-span-6 bg-white p-2 rounded-br-lg border-2 border-t border-african_violet-700 mt-1">
+          <div
+            ref={chartContainerRef}
+            className="col-span-10 h-64 rounded-t-lg overflow-hidden border-b-2 border-african_violet-700"
+          ></div>
+          <div
+            ref={stochasticChartContainerRef}
+            className="col-span-10 h-32 mt-2 rounded-b-lg overflow-hidden border-t-2 border-african_violet-700"
+          ></div>
         </div>
 
         <div className="col-span-4">
-          <div id="box-cambiadora" className="flex flex-col pt-1">
+          <div id="box-cambiadora" className="flex h-50 flex-col pt-1">
+            <img src={alarmImage} alt="Alarm Banner" className="w-full max-h-60 object-cover" />
             {renderTabContent()}
           </div>
         </div>
-      </div>      
+      </div>
     </div>
   );
 };
