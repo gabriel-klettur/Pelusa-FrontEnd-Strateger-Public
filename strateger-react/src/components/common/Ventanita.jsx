3d0606dbda2fd913bfb9150fc59a36bbd7782940
Gabriel Roca <blast.blackstone@gmail.com>
2024-08-10 22:06:41 +0000

    feat: [Important] Creacion de componente Ventanita
    - Ventanita nos ayudara a Estandarizar un poco las ventanas con informacion.

---
+++ strateger-react/src/components/common/Ventanita.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Ventanita = ({ titulo, contenido }) => {
+  return (
+    <div className="w-full mx-auto rounded-lg">
+      <div className="grid grid-rows-[auto,1fr] gap-2">
+        {/* Sección del Título */}
+        <div className="bg-african_violet-300 text-white px-4 py-2 font-semibold text-lg">
+          {titulo}
+        </div>
+        
+        {/* Sección de Contenido */}
+        <div className="bg-african_violet-100 text-african_violet-900 px-6 py-4">
+          {contenido}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ventanita;
