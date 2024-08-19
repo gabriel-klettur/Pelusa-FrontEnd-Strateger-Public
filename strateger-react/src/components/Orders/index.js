e26a092ad158eb0147c39a65f2eb69c49520f92b
Gabriel Roca <blast.blackstone@gmail.com>
2024-08-19 19:19:15 +0000

    Update index.js

--- strateger-react/src/components/Orders/index.js
+++ strateger-react/src/components/Orders/index.js
@@ -2,8 +2,9 @@
 
 export { default as Orders } from './Orders';
 export { default as OrderList } from './OrderList/OrderList';
-export { default as FilteredOrderList } from './FilteredOrderList/FilteredOrderList';
 export { default as ToolOrderBar } from './components/ToolOrderBar/ToolOrderBar';
+
+export { default as FilteredOrderList } from './FilteredOrderList/FilteredOrderList';
 export { default as OrderRow } from './OrderList/OrderRow';
 export { default as FilteredOrderRow } from './FilteredOrderList/FilteredOrderRow';
 export { default as FilterButton } from './ToolOrderBar/FilterButton';
