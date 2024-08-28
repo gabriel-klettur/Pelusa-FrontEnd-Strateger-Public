31896a11a2e6dd8987f526a5491198052d2b539e
Gabriel Roca <blast.blackstone@gmail.com>
2024-08-28 21:41:06 +0000

    test: AlarmRow and AlarmTable

--- strateger-react/src/components/Alarms/components/AlarmTable/AlarmRow.js
+++ strateger-react/src/components/Alarms/components/AlarmTable/AlarmRow.js
@@ -2,6 +2,18 @@
 
 import React from 'react';
 
+// AlarmRow component, represents a row in the alarm table
+
+
+/**
+ * Represents a row in the AlarmTable component.
+ *
+ * @param {Object} props - The props for the AlarmRow component.
+ * @param {Object} props.alarm - The alarm object.
+ * @param {boolean} props.isSelected - Indicates if the alarm is selected.
+ * @param {Function} props.handleSelectAlarm - The function to handle selecting an alarm.
+ * @returns {JSX.Element} The rendered AlarmRow component.
+ */
 const AlarmRow = ({ alarm, isSelected, handleSelectAlarm }) => {
   
   const alarmValues = [
