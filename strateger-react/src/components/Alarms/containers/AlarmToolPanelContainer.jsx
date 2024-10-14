d5df6ed4669768cad796dcb5425a3dd3dc3eb31d
Gabriel Roca <blast.blackstone@gmail.com>
2024-10-14 14:10:50 +0000

    chore(indenting): Fix indentation

--- strateger-react/src/components/Alarms/containers/AlarmToolPanelContainer.jsx
+++ strateger-react/src/components/Alarms/containers/AlarmToolPanelContainer.jsx
@@ -2,13 +2,13 @@ import AlarmToolPanel from "../components/AlarmToolPanel/AlarmToolPanel";
 import { useDispatch, useSelector } from 'react-redux';
 
 import { 
-    incrementTemporalidad, 
-    decrementTemporalidad,   
-    setSelectedTemporalidad, 
-    setSelectedTypes, 
-    selectSelectedTemporalidad, 
-    selectSelectedTypes 
-  } from '../../../redux/slices/alarmFilterSlice';
+        incrementTemporalidad, 
+        decrementTemporalidad,   
+        setSelectedTemporalidad, 
+        setSelectedTypes, 
+        selectSelectedTemporalidad, 
+        selectSelectedTypes 
+    } from '../../../redux/slices/alarmFilterSlice';
   
 
 const AlarmToolPanelContainer = () => {
@@ -26,22 +26,22 @@ const AlarmToolPanelContainer = () => {
 
     const selectedTemporalidad = useSelector(selectSelectedTemporalidad);       // temporalidad seleccionada en el panel
     const selectedTypes = useSelector(selectSelectedTypes);                     // objeto que guarda la temporalidad y los tipos seleccionados en el panel  
-  
+
     const toggleType = (type) => {
-      const types = selectedTypes[selectedTemporalidad] || [];
-      let updatedTypes;
-      if (types.includes(type)) {
-        updatedTypes = types.filter(t => t !== type);
-        dispatch(decrementTemporalidad(selectedTemporalidad));
-      } else {
-        updatedTypes = [...types, type];
-        dispatch(incrementTemporalidad(selectedTemporalidad));
-      }
-      dispatch(setSelectedTypes(updatedTypes));
+        const types = selectedTypes[selectedTemporalidad] || [];
+        let updatedTypes;
+        if (types.includes(type)) {
+            updatedTypes = types.filter(t => t !== type);
+            dispatch(decrementTemporalidad(selectedTemporalidad));
+        } else {
+            updatedTypes = [...types, type];
+            dispatch(incrementTemporalidad(selectedTemporalidad));
+        }
+        dispatch(setSelectedTypes(updatedTypes));
     };
-  
+
     const toggleTemporalidad = (temp) => {
-      dispatch(setSelectedTemporalidad(temp));
+        dispatch(setSelectedTemporalidad(temp));
     };
 
     return (        
