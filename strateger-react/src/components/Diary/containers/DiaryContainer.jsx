b546838646cd50d696e6cbaa9715ac7f6284d42e
Gabriel Roca <blast.blackstone@gmail.com>
2024-10-29 21:24:47 +0000

    Refactor(Diary): Created container to delete 'DiaryList'.
    - Moved responsibilities from 'DiaryContainer' to 'DiaryListContainer'

--- strateger-react/src/components/Diary/containers/DiaryContainer.jsx
+++ strateger-react/src/components/Diary/containers/DiaryContainer.jsx
@@ -1,28 +1,23 @@
 
 import DiaryEntryForm from "../components/DiaryEntryForm/DiaryEntryForm";
 import useDiary from '../hooks/useDiary';
-import DiaryList from '../components/DiaryList/DiaryList';
+import DiaryListContainer from "./DiaryListContainer";
 
 const DiaryContainer = () => {
-
-    const { entries, handleDeleteEntry, handleEditEntry } = useDiary();
+    
     const { editingEntry, handleAddOrUpdateEntry, handleCancelEdit } = useDiary();
-
+    
     return(
         <div className="grid grid-cols-10 bg-african_violet-400">
-            <div className="col-span-4 p-2">        {/* COLUMN LEFT */}        
+            <div className="col-span-4 p-1">                    {/* COLUMN LEFT */}
                 <DiaryEntryForm
                     onSave={handleAddOrUpdateEntry}
                     entry={editingEntry}
                     onCancelEdit={handleCancelEdit}
                 />                          
             </div>      
-            <div className="col-span-6 p-2">        {/* COLUMN RIGHT */}
-                <DiaryList 
-                    entries={entries} 
-                    onEdit={handleEditEntry} 
-                    onDelete={handleDeleteEntry} 
-                />         
+            <div className="col-span-6 pt-1 pr-1 pb-1">        {/* COLUMN RIGHT */}
+                <DiaryListContainer />         
             </div>
         </div>
     );
---
+++ strateger-react/src/components/Diary/containers/DiaryListContainer.jsx
@@ -0,0 +1,38 @@
+import Ventanita from "../../common/Ventanita";
+import DiaryEntry from "../components/DiaryList/DiaryEntry/DiaryEntry";
+import useDiary from '../hooks/useDiary';
+
+const DiaryListContainer = () => {
+
+    const { entries, handleDeleteEntry, handleEditEntry } = useDiary();
+
+    return (
+        <div>
+          <Ventanita
+            titulo="Diary Entries"
+            contenido={      
+              entries.length === 0 ? (
+                <p>No entries found.</p>
+              ) : (
+                entries.map((entry) => (
+                  <div key={`diary-entry-${entry.id}`} className='mb-4'>
+                    <Ventanita
+                      titulo={`Diary Entry ${entry.id}`}
+                      contenido={
+                        <DiaryEntry
+                          key={`diary-entry-${entry.id}`}
+                          entry={entry}
+                          onEdit={handleEditEntry}
+                          onDelete={handleDeleteEntry}
+                        />
+                      }
+                    />
+                  </div>
+                ))
+              )}    
+          />
+        </div>
+    );
+}
+
+export default DiaryListContainer;
