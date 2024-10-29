4020278b9784db3342f133177bf0ccf5182ee6cc
Gabriel Roca <blast.blackstone@gmail.com>
2024-10-29 21:19:37 +0000

    fix(Diary): Removed components that doesn't need to be exported

--- strateger-react/src/components/Diary/index.js
+++ strateger-react/src/components/Diary/index.js
@@ -1,6 +1,4 @@
 // Path: strateger-react/src/components/Diary/index.js
 
 export { default as Diary } from './Diary';
-export { default as DiaryEntryForm } from './components/DiaryEntryForm/DiaryEntryForm';
-export { default as DiaryList } from './components/DiaryList/DiaryList';
-export { default as DiaryCalendar } from './components/DiaryCalendar/DiaryCalendar';
+
