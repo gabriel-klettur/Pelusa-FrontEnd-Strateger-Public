c7d3d493165ca9a47c5f73ee893c5beca061b7b8
Gabriel Roca <blast.blackstone@gmail.com>
2024-10-30 08:56:58 +0000

    feat(Diary): Added CandlestickChart in 'DiaryContainer'

--- strateger-react/src/components/Diary/containers/DiaryContainer.jsx
+++ strateger-react/src/components/Diary/containers/DiaryContainer.jsx
@@ -2,22 +2,28 @@
 import DiaryEntryForm from "../components/DiaryEntryForm/DiaryEntryForm";
 import useDiary from '../hooks/useDiary';
 import DiaryListContainer from "./DiaryListContainer";
+import CandlestickChart from "../../Charts/CandlestickChartChart/CandlestickChart";
 
 const DiaryContainer = () => {
     
     const { editingEntry, handleAddOrUpdateEntry, handleCancelEdit } = useDiary();
     
     return(
-        <div className="grid grid-cols-10 bg-african_violet-400">
-            <div className="col-span-4 p-1">                    {/* COLUMN LEFT */}
-                <DiaryEntryForm
-                    onSave={handleAddOrUpdateEntry}
-                    entry={editingEntry}
-                    onCancelEdit={handleCancelEdit}
-                />                          
-            </div>      
-            <div className="col-span-6 pt-1 pr-1 pb-1">        {/* COLUMN RIGHT */}
-                <DiaryListContainer />         
+        <div className="flex flex-col bg-african_violet-500">
+            <div className="p-1">
+                <CandlestickChart />                                {/* CANDLESTICK CHART */}
+            </div>
+            <div className="grid grid-cols-10">
+                <div className="col-span-4 p-1">                    {/* COLUMN LEFT */}
+                    <DiaryEntryForm
+                        onSave={handleAddOrUpdateEntry}
+                        entry={editingEntry}
+                        onCancelEdit={handleCancelEdit}
+                    />                          
+                </div>      
+                <div className="col-span-6 pt-1 pr-1 pb-1">        {/* COLUMN RIGHT */}
+                    <DiaryListContainer />         
+                </div>
             </div>
         </div>
     );
