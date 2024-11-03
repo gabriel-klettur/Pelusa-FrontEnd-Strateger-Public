e2b477d6d41d5389be723d5aa574d5c6cfa41b4b
Gabriel Roca <blast.blackstone@gmail.com>
2024-11-03 13:22:39 +0000

    feat(thunks): Created LoadingSlices component and modified  loadSlices dispatch component

--- strateger-react/src/App.js
+++ strateger-react/src/App.js
@@ -1,23 +1,27 @@
 // Path: strateger-react/src/App.js
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { loadSlicesInOrder } from './thunks/loadSlices';
 import MainContainer from './containers/MainContainer';
+import LoadingSlices from './components/common/LoadingSlices/LoadingSlices';
 
 const App = () => {
     const dispatch = useDispatch();
-    
+    const [loadingMessage, setLoadingMessage] = useState('Loading...');
+    const [isLoadingVisible, setIsLoadingVisible] = useState(true); // Estado para la visibilidad del div
+
     //!------------ Load Redux Slices in Order -------------
     useEffect(() => {
-      dispatch(loadSlicesInOrder());
+      dispatch(loadSlicesInOrder(setLoadingMessage));
     }, [dispatch]);
     //!-----------------------------------------------------
 
     return (        
-        <>
-          <MainContainer/>
-        </>             
+      <>
+        <LoadingSlices isLoadingVisible={isLoadingVisible} loadingMessage={loadingMessage} setIsLoadingVisible={setIsLoadingVisible}/>          
+        <MainContainer/>
+      </>             
     );
 };
 
--- strateger-react/src/thunks/loadSlices.js
+++ strateger-react/src/thunks/loadSlices.js
@@ -1,43 +1,60 @@
 // Path: src/thunks/loadSlices.js
 
-//import { fetchAlarms } from '../slices/alarmSlice';
-//import { fetchOrders } from '../slices/orderSlice';
 import { fetchStrategies } from '../redux/strategy';
 import { fetchDiaryEntries } from '../redux/diary';
-import { fetchTrackRecordBingXAllAccounts } from '../redux/account';
+import { fetchPerpCOINMBalance,fetchPerpUSDTMBalance,fetchSpotBalance, fetchTrackRecordBingXAllAccounts } from '../redux/account';
 import { fetchPositionsCoinM, fetchPositionsUSDTM } from '../redux/position';
 import { fetchCandlestickChartData } from '../redux/charts';
-import { fetchTicker } from '../redux/ticker'; // Importa fetchTicker
+import { fetchTicker } from '../redux/ticker';
+import { fetchOrdersCoinm, fetchOrdersSpot, fetchOrdersStandard, fetchOrdersUsdtm } from '../redux/order';
 
-export const loadSlicesInOrder = () => async (dispatch) => {
-  try {
-
+export const loadSlicesInOrder = (setLoadingMessage) => async (dispatch) => {
+  try {    
+    setLoadingMessage('Loading minimun information');
+    //TODO -------------- fetch Basics --------------------------------------
     await Promise.all([
       dispatch(fetchTicker('ETH-USDT')),
       dispatch(fetchTicker('BTC-USDT'))
-    ]);
+    ]);    
 
+    setLoadingMessage('Loading Charts information');
+    //TODO -------------- fetch Charts --------------------------------------
     await dispatch(fetchCandlestickChartData({
       interval: '1d',
       startDate: new Date(Date.now() - 1000 * 60 * 60 * 24 * 1000).toISOString(), // 1000 days ago
       endDate: new Date().toISOString()
     }));
-    
-    //await dispatch(fetchAlarms({ limit: 100, offset: 0 }));
-    //await dispatch(fetchOrders({ limit: 100, offset: 0 }));
+      
+    setLoadingMessage('Loading Orders information');
+    //TODO -------------- fetch Orders --------------------------------------
+    await dispatch(fetchOrdersCoinm({ limit: 500, offset: 0 }));
+    await dispatch(fetchOrdersSpot({ limit: 500, offset: 0 }));
+    await dispatch(fetchOrdersStandard({ limit: 500, offset: 0 }));
+    await dispatch(fetchOrdersUsdtm({ limit: 500, offset: 0 }));
 
-    await dispatch(fetchStrategies({ skip: 0, limit: 10 }));
-    await dispatch(fetchDiaryEntries({ skip: 0, limit: 10 }));
-
-
-    //await dispatch(fetchPerpUSDTMBalance()); // Dependent on CandlestickChart
-    //await dispatch(fetchPerpCOINMBalance()); // Dependent on CandlestickChart
-    //await dispatch(fetchSpotBalance());     // Dependent on CandlestickChart
+    setLoadingMessage('Loading Account information');
+    //TODO -------------- fetch Account --------------------------------------
+    await dispatch(fetchPerpCOINMBalance());
+    await dispatch(fetchPerpUSDTMBalance());
+    await dispatch(fetchSpotBalance());
     await dispatch(fetchTrackRecordBingXAllAccounts());
 
+    setLoadingMessage('Loading Strategy information');
+    //TODO -------------- fetch Strategies --------------------------------------
+    await dispatch(fetchStrategies({ skip: 0, limit: 10 }));
+
+    setLoadingMessage('Loading Diary information');
+    //TODO -------------- fetch Diary --------------------------------------
+    await dispatch(fetchDiaryEntries({ skip: 0, limit: 10 }));
+    
+    setLoadingMessage('Loading Positions information');
+    //TODO -------------- fetch Positions --------------------------------------
     await dispatch(fetchPositionsCoinM());
-    await dispatch(fetchPositionsUSDTM());    
+    await dispatch(fetchPositionsUSDTM()); 
+
+    setLoadingMessage('All Redux slices loaded');
   } catch (error) {
+    setLoadingMessage('Error loading slices');
     console.error('Error loading slices:', error);
   }
 };
