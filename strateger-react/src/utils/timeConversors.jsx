c5a2a5a8020094bd11ffdeab94a59be56e8ddd4a
Gabriel Roca <blast.blackstone@gmail.com>
2024-11-07 20:49:36 +0000

    Create timeConversors.jsx

---
+++ strateger-react/src/utils/timeConversors.jsx
@@ -0,0 +1,18 @@
+
+//String format standard in Strateger Products is SQL format 'YYYY-MM-DD HH:MM:SS'
+
+
+//Convert ISO 8601 date to a string in the format 'YYYY-MM-DD HH:MM:SS'
+
+
+//Convert Timestamp date to a string in the format 'YYYY-MM-DD HH:MM:SS'
+
+
+//Convert string date with format 'YYYY-MM-DD HH:MM:SS' to ISO 8601 date
+
+
+//Convert string date with format 'YYYY-MM-DD HH:MM:SS' to Timestamp date
+
+
+
+
