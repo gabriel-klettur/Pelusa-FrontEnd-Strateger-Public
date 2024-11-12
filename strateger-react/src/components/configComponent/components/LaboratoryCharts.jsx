de81e5a0a5acdaecc1cd141f62baec1ba245ab69
Gabriel Roca <blast.blackstone@gmail.com>
2024-11-12 12:35:03 +0000

    Style(Config): Changed height structure

--- strateger-react/src/components/configComponent/components/LaboratoryCharts.jsx
+++ strateger-react/src/components/configComponent/components/LaboratoryCharts.jsx
@@ -4,12 +4,14 @@ import Ventanita from '../../common/Ventanita';
 const LaboratoryCharts = () => {
     return(
         <div className="flex flex-col mt-2">
-            <Ventanita
-                titulo='For Alarms'
-                contenido={
-                    <VerticalBarChartV2 />
-                }
-            />
+            <div className='h-32'>
+                <Ventanita
+                    titulo='For Alarms'
+                    contenido={
+                        <VerticalBarChartV2 />
+                    }
+                />
+            </div>
         </div>
     )
 }
