179b73e958704c2124e3796e3855bb2bec6a9713
Gabriel Roca <blast.blackstone@gmail.com>
2024-11-18 18:37:26 +0000

    feat(ToolBar): Deleted unnecessary 'DatePicker'

--- strateger-react/src/components/ToolBar/containers/ToolBarContainer.jsx
+++ strateger-react/src/components/ToolBar/containers/ToolBarContainer.jsx
@@ -5,7 +5,6 @@ import { useSelector } from 'react-redux';
 
 import IntervalBarContainer from './IntervalBarContainer';
 import RelojContainer from './RelojContainer';
-import DatePickerContainer from './DatePickerContainer';
 
 import { selectTemporalidad, selectCurrentDate, selectStartDate } from '../../../redux/toolBar';
 
@@ -23,7 +22,7 @@ const ToolBarContainer = () => {
     }, [startDate]);
    
     return(
-        <div className="h-14 grid grid-cols-3 gap-4 bg-african_violet-300">
+        <div className="h-14 grid grid-cols-2 gap-4 bg-african_violet-300">
             <div className="h-full flex justify-center items-center ">
                 <IntervalBarContainer
                     currentInterval={currentInterval}
@@ -32,13 +31,6 @@ const ToolBarContainer = () => {
                     endDate={endDate}
                 />   
             </div>             
-            <div className="h-full flex justify-center items-center">
-                <DatePickerContainer
-                    setLocalDate={setLocalDate}
-                    currentInterval={currentInterval}            
-                    localDate={localDate}                
-                />      
-            </div>
             <div className="h-full flex justify-end items-center">
                 <RelojContainer />
             </div>
