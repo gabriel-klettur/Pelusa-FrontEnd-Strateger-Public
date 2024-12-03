90581f3e31e845cf26fc078837fa7515d6ab90c1
Gabriel Roca <blast.blackstone@gmail.com>
2024-12-03 10:04:58 +0000

    Fix(Alarms): Fixed problem with comparation between 'alarm.Order' and 'orderType'
    - State name was wrong
    - 'orderType' before use UpperCase

--- strateger-react/src/components/Alarms/containers/AlarmFiltersPanelContainer.jsx
+++ strateger-react/src/components/Alarms/containers/AlarmFiltersPanelContainer.jsx
@@ -21,18 +21,59 @@ const AlarmFiltersPanelContainer = () => {
   const handleApplyFilters = (filters) => {
     const { intervals, ordersType, strategies, tickers } = filters;
 
+    console.log('--------------------------------')
+    console.log('alarms', alarms);
+    console.log('intervals', intervals);
+    console.log('ordersType', ordersType);
+    console.log('strategies', strategies);
+    console.log('tickers', tickers);    
+    console.log('--------------------------------')
+
     const newFilteredAlarms = alarms.filter((alarm) => {
-      const matchesInterval = intervals[alarm.Temporalidad] || false;
-      const matchesOrderType = ordersType[alarm.Order] || false;
-      const matchesStrategy = strategies[alarm.Strategy] || false;
-      const matchesTicker = tickers[alarm.Ticker] || false;
+      let foundIntervalFilter = false;
+      let foundOrderTypeFilter = false;
+      let foundStrategyFilter = false;
+      let foundTickerFilter = false;
+      let totalResult = false;
 
-      console.log('matchesInterval', matchesInterval);
-      console.log('matchesOrderType', matchesOrderType);
-      console.log('matchesStrategy', matchesStrategy);
-      console.log('matchesTicker', matchesTicker);
+      const intervals = Object.keys(filters.intervals).filter(key => filters.intervals[key]);
+      const ordersType = Object.keys(filters.ordersType).filter(key => filters.ordersType[key]);
+      const strategies = Object.keys(filters.strategies).filter(key => filters.strategies[key]);
+      const tickers = Object.keys(filters.tickers).filter(key => filters.tickers[key]);      
+      
+      intervals.forEach(interval => {        
+        if (alarm.Temporalidad === interval) {
+          foundIntervalFilter = true;                    
+        }
+      });
 
-      return matchesInterval || matchesOrderType || matchesStrategy || matchesTicker;
+      ordersType.forEach(orderType => {                
+        if (alarm.Order.replace(/Order/i, '').trim() === orderType.toLowerCase()) {          
+          foundOrderTypeFilter = true;          
+        }
+      });
+
+      strategies.forEach(strategy => {        
+        if (alarm.Strategy === strategy) {
+          foundStrategyFilter = true;          
+        }
+      });
+
+      tickers.forEach(ticker => {        
+        if (alarm.Ticker === ticker) {
+          foundTickerFilter = true;          
+        }
+      });
+/*
+      console.log('foundIntervalFilter', foundIntervalFilter);
+      console.log('foundOrderTypeFilter', foundOrderTypeFilter);
+      console.log('foundStrategyFilter', foundStrategyFilter);
+      console.log('foundTickerFilter', foundTickerFilter);
+*/
+      totalResult = foundIntervalFilter || foundOrderTypeFilter || foundStrategyFilter || foundTickerFilter;
+      
+  
+      return totalResult; // Solo incluye la alarma si pasa todos los filtros.
     });
 
     dispatch(setFilteredByOptions(newFilteredAlarms));
