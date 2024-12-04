daa636ae199413808fd9ed57d183a5c86d0c1afa
Gabriel Roca <blast.blackstone@gmail.com>
2024-12-04 17:28:45 +0000

    feat(Alarms): Added possible datasets to use

--- strateger-react/src/components/Alarms/components/AlarmInfoPanel/ChartAlarmsByTime/ChartAlarmsByTime.jsx
+++ strateger-react/src/components/Alarms/components/AlarmInfoPanel/ChartAlarmsByTime/ChartAlarmsByTime.jsx
@@ -27,7 +27,21 @@ const getRadarChartData = (alarmsByHour) => {
       labels: timeLabels,
       datasets: [
         {
-          label: 'Metrics Distribution',
+          label: 'All Alarms',
+          data: alarmsByHour, 
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+        },
+        {
+          label: 'Selected Alarms',
+          data: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], 
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+        },
+        {
+          label: 'Filtered',
           data: alarmsByHour, 
           backgroundColor: 'rgba(255, 99, 132, 0.2)',
           borderColor: 'rgba(255, 99, 132, 1)',
