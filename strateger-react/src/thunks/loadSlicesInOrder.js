0b75d1987c6f7f0accfeaab3a68a2f9a833542ec
Gabriel Roca <blast.blackstone@gmail.com>
2024-12-06 09:23:47 +0000

    refactor(thunks): comment temporaly unused imports and calls in loadSlicesInOrder.js
    Commented out unused imports and function calls to optimize code execution.
    No functional changes made to the application.

--- strateger-react/src/thunks/loadSlicesInOrder.js
+++ strateger-react/src/thunks/loadSlicesInOrder.js
@@ -1,28 +1,29 @@
 
 import { toast } from 'react-toastify';
 import { loadMinimumInformation } from './loadSlices';
-import { loadChartsInformation } from './loadSlices';
-import { loadOrdersInformation } from './loadSlices';
-import { loadAccountInformation } from './loadSlices';
-import { loadStrategiesInformation } from './loadSlices';
-import { loadDiaryInformation } from './loadSlices';
-import { loadPositionsInformation } from './loadSlices';
 import { loadAlarmsInformation } from './loadSlices';
+//import { loadChartsInformation } from './loadSlices';
+//import { loadOrdersInformation } from './loadSlices';
+//import { loadAccountInformation } from './loadSlices';
+//import { loadStrategiesInformation } from './loadSlices';
+//import { loadDiaryInformation } from './loadSlices';
+//import { loadPositionsInformation } from './loadSlices';
+
 
 
 export const loadSlicesInOrder = () => async (dispatch) => {
     try {
       await loadMinimumInformation(dispatch);
       await loadAlarmsInformation(dispatch);
-      await loadChartsInformation(dispatch);
-      await loadOrdersInformation(dispatch);
-      await loadAccountInformation(dispatch);
-      await loadStrategiesInformation(dispatch);
-      await loadDiaryInformation(dispatch);
-      await loadPositionsInformation(dispatch);
+      //await loadChartsInformation(dispatch);
+      //await loadOrdersInformation(dispatch);
+      //await loadAccountInformation(dispatch);
+      //await loadStrategiesInformation(dispatch);
+      //await loadDiaryInformation(dispatch);
+      //await loadPositionsInformation(dispatch);
       
       // Mensaje final de éxito
-      toast.success('All Redux slices loaded successfully');
+      //toast.success('All Redux slices loaded successfully');
     } catch (error) {
       toast.error('Error loading slices');
       console.error('Error loading slices:', error);
