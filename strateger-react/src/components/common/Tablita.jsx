6a38894834bd526a8a13e08faad776c62bbaa896
Gabriel Roca <blast.blackstone@gmail.com>
2024-12-10 14:02:44 +0000

    feat(common/Tablita): Added conditional to detect when 'data' is empty.
    Show 'No data' message when 'data' is emtpy.

--- strateger-react/src/components/common/Tablita.jsx
+++ strateger-react/src/components/common/Tablita.jsx
@@ -1,14 +1,16 @@
 // Path: strateger-react/src/components/common/Tablita.jsx
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 /**
- * Componente para renderizar una tabla estilizada.
- * @param {Object} props - Las props del componente.
- * @param {Array} props.data - Los datos que se mostrarán en la tabla.
- * @param {Array} props.columns - Los encabezados de las columnas y las claves de datos correspondientes.
- * @param {Function} [props.renderRow] - Función opcional para renderizar filas personalizadas.
- * @returns {JSX.Element} - El componente de la tabla.
+ * Tablita component renders a table with selectable rows.
+ *
+ * @param {Object} props - The component props.
+ * @param {Array} props.data - The data to be displayed in the table.
+ * @param {Array} props.columns - The columns configuration for the table.
+ * @param {Function} [props.renderRow] - Optional custom row rendering function.
+ *
+ * @returns {JSX.Element} The rendered table component.
  */
 const Tablita = ({ data, columns, renderRow }) => {
   const [selectedRowIndex, setSelectedRowIndex] = useState(null);
@@ -25,23 +27,31 @@ const Tablita = ({ data, columns, renderRow }) => {
         </tr>
       </thead>
       <tbody>
-        {data.map((item, index) => (
-          renderRow ? renderRow(item, index) : (
-            <tr
-              key={index}
-              className={`border-b transition-colors duration-200 hover:bg-african_violet-700 cursor-pointer ${
-                selectedRowIndex === index ? 'bg-african_violet-400 text-white' : 'bg-white text-african_violet-200'
-              }`}
-              onClick={() => setSelectedRowIndex(index)}
-            >
-              {columns.map((column, colIndex) => (
-                <td key={colIndex} className="py-2 px-4 border-r">
-                  {item[column.key]}
-                </td>
-              ))}
-            </tr>
-          )
-        ))}
+        {data.length === 0 ? (
+          <tr>
+            <td colSpan={columns.length} className="py-4 px-6 text-center text-gray-500">
+              No data
+            </td>
+          </tr>
+        ) : (
+          data.map((item, index) => (
+            renderRow ? renderRow(item, index) : (
+              <tr
+                key={index}
+                className={`border-b transition-colors duration-200 hover:bg-african_violet-700 cursor-pointer ${
+                  selectedRowIndex === index ? 'bg-african_violet-400 text-white' : 'bg-white text-african_violet-200'
+                }`}
+                onClick={() => setSelectedRowIndex(index)}
+              >
+                {columns.map((column, colIndex) => (
+                  <td key={colIndex} className="py-2 px-4 border-r">
+                    {item[column.key]}
+                  </td>
+                ))}
+              </tr>
+            )
+          ))
+        )}
       </tbody>
     </table>
   );
