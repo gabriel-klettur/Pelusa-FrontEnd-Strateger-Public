89a5bd81e05a4ebf52b0a7aa5470439731a8631a
Gabriel Roca <blast.blackstone@gmail.com>
2024-12-12 20:41:40 +0000

    test(Alarms): add unit tests for Alarms component
    - Rendered Alarms component and verified the presence of the main container.
    - Verified that the AlarmsContainer child component is rendered correctly.

---
+++ strateger-react/src/components/Alarms/__tests__/Alarms.test.jsx
@@ -0,0 +1,22 @@
+// Path: src/components/Alarms/__tests__/Alarms.test.jsx
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Alarms from '../Alarms';
+
+//  Mock del componente AlarmsContainer
+jest.mock('../containers/AlarmContainer', () => () => <div data-testid="alarms-container"></div>);
+
+describe('Alarms component', () => {
+  it('should render the Alarms component and its child AlarmsContainer', () => {
+    //  Renderizar el componente
+    render(<Alarms />);
+    
+    //  Comprobar que se ha renderizado el contenedor principal
+    const alarmsComponent = screen.getByTestId('alarms-component');
+    expect(alarmsComponent).toBeInTheDocument();
+
+    // ✅Comprobar que se ha renderizado el componente hijo AlarmsContainer
+    const alarmsContainer = screen.getByTestId('alarms-container');
+    expect(alarmsContainer).toBeInTheDocument();
+  });
+});
