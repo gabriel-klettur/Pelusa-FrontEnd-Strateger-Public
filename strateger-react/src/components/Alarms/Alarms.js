bd9f2581739a0f3fa1c0c69b6ab166ee293ddac8
Gabriel Roca <blast.blackstone@gmail.com>
2024-12-12 20:20:31 +0000

    chore(Alarms): Added div data-testid for tests

--- strateger-react/src/components/Alarms/Alarms.js
+++ strateger-react/src/components/Alarms/Alarms.js
@@ -4,13 +4,10 @@ import React from 'react';
 import AlarmsContainer from './containers/AlarmContainer';
 
 const Alarms = () => {  
-
   return (
-    <>     
-       
+    <div data-testid="alarms-component">     
       <AlarmsContainer />
-      
-    </>
+    </div>
   );
 };
 
