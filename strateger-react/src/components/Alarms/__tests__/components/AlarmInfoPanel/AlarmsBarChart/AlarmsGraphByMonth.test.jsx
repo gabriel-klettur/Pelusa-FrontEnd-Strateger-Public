9b09c6b546e09bf7b6735b6e5875c2be65d0a3ff
Gabriel Roca <blast.blackstone@gmail.com>
2024-12-13 23:01:28 +0000

    fix(Alarms): fixed test folder names

--- strateger-react/src/components/Alarms/__tests__/components/AlarmInfoPanel/AlarmsBarChart/AlarmsGraphByMonth.test.jsx
+++ strateger-react/src/components/Alarms/__tests__/components/AlarmInfoPanel/AlarmsBarChart/AlarmsGraphByMonth.test.jsx
@@ -3,7 +3,7 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import AlarmsBarChart from '../../../../../Alarms/components/AlarmInfoPanel/AlarmsBarChart/AlarmsBarChart';
+import AlarmsBarChart from '../../../../components/AlarmInfoPanel/AlarmsBarChart/AlarmsBarChart';
 
 
 const mockStore = configureStore([]);
@@ -14,33 +14,6 @@ const initialState = {
         alarms: {
         data: [],
         length: 0,
-        page: 0,
-        offset: 0,
-        hasMore: true,
-        },
-        filteredByClickAlarms: {
-        data: [],
-        length: 0,
-        page: 0,
-        hasMore: true,
-        },
-        filteredByOptions: {
-        data: [],
-        length: 0,
-        page: 0,
-        hasMore: true,
-        },
-    },
-};
-
-const testStateWithData = {
-    alarms: {
-        alarms: {
-        data: [
-            { id: 1, Ticker: 'BTC', Interval: '15m', Price_Alert: 30000, Time_Alert: '2024-12-10T10:30:00Z', Order: 'Long', Strategy: 'Test' },
-            { id: 2, Ticker: 'ETH', Interval: '5m', Price_Alert: 2000, Time_Alert: '2024-12-11T12:00:00Z', Order: 'Short', Strategy: 'Demo' },
-        ],
-        length: 2,
         page: 0,
         offset: 0,
         hasMore: true,
