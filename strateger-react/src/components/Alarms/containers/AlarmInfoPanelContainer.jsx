784e69cb7b119bd5d72ee9004a43c22e7686f5e1
Gabriel Roca <blast.blackstone@gmail.com>
2024-12-13 20:23:37 +0000

    Revert "refactor(alarms): modularize AlarmInfoPanel logic and create reusable hook for alarms hourly dataset"
    This reverts commit 5717be5597157d69d4d2b2421ab4f552fce040d5.

--- strateger-react/src/components/Alarms/containers/AlarmInfoPanelContainer.jsx
+++ strateger-react/src/components/Alarms/containers/AlarmInfoPanelContainer.jsx
@@ -1,20 +1,46 @@
 //Path: src/components/Alarms/containers/AlarmInfoPanel.jsx
 
+//import ChartAlarmsByMonth from '../components/AlarmInfoPanel/ChartAlarmsByMonth/ChartAlarmsByMonth';
+
 import { TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
 import AlarmTab from '../components/AlarmTab';
 import ChartAlarmsByMonth from '../components/AlarmInfoPanel/AlarmsGraphByMonth/AlarmsGraphByMonth';
 import ChartAlarmsByTime from '../components/AlarmInfoPanel/AlarmsGraphByTime/ChartAlarmsByTime';
 import GeneralStatistics from '../components/AlarmInfoPanel/GeneralStatistics/GeneralStatistics';
 
-import useAlarmsHourlyDataset  from '../hooks/useAlarmsHourlyDataset';
-
 const AlarmInfoPanelContainer = ({alarmsData, filteredByClickAlarmsData, filteredByOptionsAlarmsData}) => {    
 
-    //TODO - Create the datasets for the Radar Chart
-    const { alarmsByHourArray, alarmsByHourFilteredByClickArray, alarmsByHourFilteredByOptionsArray } = useAlarmsHourlyDataset(alarmsData, filteredByClickAlarmsData, filteredByOptionsAlarmsData);
+    const alarmsByHour = alarmsData.reduce((acc, alarm) => {
+        if (alarm.Time_Alert) {
+          const hour = new Date(alarm.Time_Alert).getHours();   // Extract the hour from Time_Alert
+          acc[hour] = (acc[hour] || 0) + 1;                     // Incrementa el contador para esa hora
+        }
+        return acc;
+    }, {});        
+    const alarmsByHourArray = Object.values(alarmsByHour);
+
+    const alarmsByHourFilteredByClick = filteredByClickAlarmsData.reduce((acc, alarm) => {
+        if (alarm.Time_Alert) {
+          const hour = new Date(alarm.Time_Alert).getHours(); // Extrae la hora de Time_Alert
+          acc[hour] = (acc[hour] || 0) + 1; // Incrementa el contador para esa hora
+        }
+        return acc;
+    }, {});
+    const alarmsByHourFilteredByClickArray = Object.values(alarmsByHourFilteredByClick);
+
+    const alarmsByHourFilteredByOptions = filteredByOptionsAlarmsData.reduce((acc, alarm) => {
+        if (alarm.Time_Alert) {
+          const hour = new Date(alarm.Time_Alert).getHours(); // Extrae la hora de Time_Alert
+          acc[hour] = (acc[hour] || 0) + 1; // Incrementa el contador para esa hora
+        }
+        return acc;
+    }, {});
+    const alarmsByHourFilteredByOptionsArray = Object.values(alarmsByHourFilteredByOptions);
+    
 
     return(
-        <div className='h-full bg-african_violet-200 p-2' data-testid="alarm-info-panel-container">                                        
+        <div className='h-full bg-african_violet-200 p-2' data-testid="alarm-info-panel-container">
+            {/*<ChartAlarmsByMonth />                                */}            
             <TabGroup data-testid="alarm-info-tab-group">
                 <TabList className="flex justify-center w-full " data-testid="alarm-info-tab-list">
                     <AlarmTab
@@ -47,6 +73,8 @@ const AlarmInfoPanelContainer = ({alarmsData, filteredByClickAlarmsData, filtere
                     </TabPanel>
                 </TabPanels>
             </TabGroup>
+    
+
         </div>
     )
 }
