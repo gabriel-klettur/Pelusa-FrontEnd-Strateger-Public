04ff4526c7f072b61c647d63027aa0973128873a
Gabriel Roca <blast.blackstone@gmail.com>
2024-12-18 12:22:40 +0000

    Fix(Alarms): Fixed checkbox in 'FiltersMenu'
    - Previously, the checkbox was not working properly. The click responsibility was moved to the entire div containing the checkbox. As a result, both the checkbox and the div now respond correctly to clicks.

--- strateger-react/src/components/Alarms/components/AlarmFiltersPanel/FiltersMenu/CheckBox.jsx
+++ strateger-react/src/components/Alarms/components/AlarmFiltersPanel/FiltersMenu/CheckBox.jsx
@@ -3,13 +3,7 @@ import CheckIcon from '../../../assets/check_icon.svg';
 const CheckBox = ({ isChecked, callback }) => {
   return (
     <div
-      className={`checkbox ${
-        isChecked ? 'checkbox-checked' : 'checkbox-unchecked'
-      } `}
-      onClick={(e) => {
-        e.stopPropagation();    // avoid propagation of the event
-        callback();              
-      }}
+      className={`checkbox ${isChecked ? 'checkbox-checked' : 'checkbox-unchecked'} `}
     >
       {isChecked && (
         <img
