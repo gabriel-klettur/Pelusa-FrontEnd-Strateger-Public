2b0ee794e09b3524890078bdd4df6c7512becd06
Gabriel Roca <blast.blackstone@gmail.com>
2025-01-02 09:25:23 +0000

    chore(Alarms/index.jsx): Added comments to split 'exports'

--- strateger-react/src/components/Alarms/index.jsx
+++ strateger-react/src/components/Alarms/index.jsx
@@ -1,6 +1,11 @@
 //Path: src/components/Alarms/index.jsx
 
 export { default as Alarms } from './Alarms';
+
+//!-------------------------------------------------------------
+//!------------------------ For Testing ------------------------
+//!-------------------------------------------------------------
+
 export { default as AlarmTable } from './components/AlarmTable/AlarmTable';
 export { default as AlarmFiltersPanelContainer } from './containers/AlarmFiltersPanelContainer';
 
