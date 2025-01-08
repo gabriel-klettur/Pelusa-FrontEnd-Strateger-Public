ad7f2b3a173f52cd8f4e17ee506e977277e7ed22
Gabriel Roca <blast.blackstone@gmail.com>
2025-01-08 15:59:02 +0000

    chore(NavBar): Changed orders of 'tabs'

--- strateger-react/src/components/NavBar/containers/NavBarContainer.jsx
+++ strateger-react/src/components/NavBar/containers/NavBarContainer.jsx
@@ -24,20 +24,20 @@ import LaboratoryIcon from '../assets/icons/lab.svg';
 
 // Define the tabs and their corresponding icons
 const tabs = [    
-    { name: 'Alarmas',          icon: AlarmsIcon,       route:'/alarms',        disabled: false },
-    { name: 'Battle Field',     icon: battleFieldIcon,  route:'/battleField',   disabled: true },
-    { name: 'Órdenes',          icon: OrdersIcon,       route:'/Orders',        disabled: true },
-    { name: 'Estrategias',      icon: StrategyIcon,     route:'/Strategy',      disabled: true },
-    { name: 'Diario',           icon: DiaryIcon,        route:'/Diary',         disabled: true },
-    { name: 'Account',          icon: AccountIcon,      route:'/Account',       disabled: true },
-    { name: 'Positions',        icon: PositionIcon,     route:'/Position',      disabled: true },
-    { name: 'Backtesting',      icon: BacktestingIcon,  route:'/Backtesting',   disabled: true },
-    { name: 'Earnings',         icon: EarningsIcon,     route:'/Earnings',      disabled: true },
-    { name: 'News',             icon: NewsIcon,         route:'/News',          disabled: true },
-    { name: 'Divisas',          icon: DivisasIcon,      route:'/Divisas',       disabled: true },
-    { name: 'Reina',            icon: ReinaIcon,        route:'/Share',         disabled: true },
-    { name: 'Laboratorio',      icon: LaboratoryIcon,   route:'/Lav',           disabled: true },
-    { name: 'Configuración',    icon: ConfigIcon,       route:'/Config',        disabled: true },
+    { name: 'Alarmas',          icon: AlarmsIcon,       route:'/pelusa-trader/alarms',        disabled: false },
+    { name: 'Estrategias',      icon: StrategyIcon,     route:'/pelusa-trader/Strategy',      disabled: true },
+    { name: 'Battle Field',     icon: battleFieldIcon,  route:'/pelusa-trader/battleField',   disabled: true },
+    { name: 'Órdenes',          icon: OrdersIcon,       route:'/pelusa-trader/Orders',        disabled: true },    
+    { name: 'Diario',           icon: DiaryIcon,        route:'/pelusa-trader/Diary',         disabled: true },
+    { name: 'Account',          icon: AccountIcon,      route:'/pelusa-trader/Account',       disabled: true },
+    { name: 'Positions',        icon: PositionIcon,     route:'/pelusa-trader/Position',      disabled: true },
+    { name: 'Backtesting',      icon: BacktestingIcon,  route:'/pelusa-trader/Backtesting',   disabled: true },
+    { name: 'Earnings',         icon: EarningsIcon,     route:'/pelusa-trader/Earnings',      disabled: true },
+    { name: 'News',             icon: NewsIcon,         route:'/pelusa-trader/News',          disabled: true },
+    { name: 'Divisas',          icon: DivisasIcon,      route:'/pelusa-trader/Divisas',       disabled: true },
+    { name: 'Reina',            icon: ReinaIcon,        route:'/pelusa-trader/Share',         disabled: true },
+    { name: 'Laboratorio',      icon: LaboratoryIcon,   route:'/pelusa-trader/Lav',           disabled: true },
+    { name: 'Configuración',    icon: ConfigIcon,       route:'/pelusa-trader/Config',        disabled: true },
 ];
 
 const NavBarContainer = () => {
