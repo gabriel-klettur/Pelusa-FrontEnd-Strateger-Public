fdb20e978e8ff0ef2e1f3b44a9a8d63749186024
Gabriel Roca <blast.blackstone@gmail.com>
2025-01-13 20:34:33 +0000

    chore(ToolBar): Removed unnecessary and unused props
    This props are dispatched with redux inside of 'ToolBarContainer'

---
+++ strateger-react/src/components/ToolBar/Toolbar.jsx
@@ -0,0 +1,11 @@
+// Path: src/components/ToolBar/Toolbar.js
+
+import ToolBarContainer from "./containers/ToolBarContainer";
+
+const Toolbar = () => {
+    return (
+        <ToolBarContainer/>
+    );
+};
+
+export default Toolbar;
