29268864f29e24b04e851a109ae040e6fe21e10d
Gabriel Roca <blast.blackstone@gmail.com>
2025-01-14 14:55:26 +0000

    feat(ToolBar): Adapted ToolBarContainer for the new subcomponents

--- strateger-react/src/components/ToolBar/containers/ToolBarContainer.jsx
+++ strateger-react/src/components/ToolBar/containers/ToolBarContainer.jsx
@@ -1,33 +1,67 @@
 //Path: strateger-react/src/components/ToolBar/containers/ToolBarContainer.jsx
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import IntervalBarContainer from './IntervalBarContainer';
 import RelojContainer from './RelojContainer';
 
 import { selectTemporalidad, selectCurrentDate, selectStartDate } from 'reduxStore/toolBar';
+import JumpInTimePanel from '../components/JumpInTimePanel';
+import IndicatorsPanel from '../components/IndicatorsPanel';
+import TickersPanel from '../components/TickersPanel';
 
 const ToolBarContainer = () => {
 
     const initialTemporalidad = useSelector(selectTemporalidad);
+    //!const initialTicker = useSelector(selectTicker);
     const startDate = useSelector(selectStartDate);
     const endDate = useSelector(selectCurrentDate);
 
     const [currentInterval, setCurrentInterval] = useState(initialTemporalidad); 
+    const [currentTicker, setCurrentTicker] = useState('BTCUSDT');
+    const [jumpToDate, setJumpToDate] = useState(null);    
+
+    
+    
+    useEffect(() => {
+        console.log('currentInterval:', currentInterval);
+        console.log('currentTicker:', currentTicker);
+        console.log('jumpToDate:', jumpToDate);
+    }, [currentInterval, currentTicker, jumpToDate]);
+
+
 
 
     return(
-        <div className="h-14 grid grid-cols-2 gap-4 bg-african_violet-300">
-            <div className="h-full flex justify-center items-center ">
+        <div className="h-14 grid grid-flow-col auto-cols-auto gap-x-4 bg-african_violet-300">
+            <div className="h-full flex justify-start items-center border-yellow-500 border-4">
+                <TickersPanel 
+                    currentTicker={currentTicker}
+                    setCurrentTicker={setCurrentTicker}
+                />    
+            </div>
+
+            <div className="h-full flex justify-center items-center border-green-500 border-4">
                 <IntervalBarContainer
                     currentInterval={currentInterval}
                     setCurrentInterval={setCurrentInterval}
                     startDate={startDate}
                     endDate={endDate}
                 />   
-            </div>             
-            <div className="h-full flex justify-end items-center">
+            </div>
+
+            <div className="h-full flex justify-center items-center border-blue-500 border-4">
+                <JumpInTimePanel
+                    setJumpToDate={setJumpToDate}
+                />
+            </div>
+
+            <div className="h-full flex justify-center items-center border-red-500 border-4">
+                <IndicatorsPanel />
+            </div>
+
+            <div className="h-full flex justify-end items-center border-cyan-500 border-4">
                 <RelojContainer />
             </div>
         </div>
