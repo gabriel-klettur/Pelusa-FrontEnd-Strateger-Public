16bdf1cee7070dd5f4085ef259c1f9dc3d932710
Gabriel Roca <blast.blackstone@gmail.com>
2025-01-25 14:58:30 +0000

    refactor(Charts/MainCharts): Responsabilities for candlestick chart was moved to 'CandlestickChartContainer'

--- strateger-react/src/components/Charts/MainChart/containers/CandlestickChartContainer.jsx
+++ strateger-react/src/components/Charts/MainChart/containers/CandlestickChartContainer.jsx
@@ -1,10 +1,36 @@
 // Path: strateger-react/src/components/Charts/CandlestickChartChart/containers/CandlestickChartContainer.js
 
-const CandlestickChartContainer = ({chartContainerRef}) => {  
+import { useRef } from 'react';
+
+import useSetupMarkers from '../hooks/useSetupMarkers';
+import useInitializeChart from '../hooks/useInitializeChart';
+import useInitializeCandlestickSeries from '../hooks/useInitializeCandlestickSeries';
+import useInitializeEmasSeries from '../hooks/useInitializeEmasSeries';
+import useSetCandlestickSeriesData from '../hooks/useSetCandlestickSeriesData';
+import useSetEmasSeriesData from '../hooks/useSetEmasSeriesData';
+
+const CandlestickChartContainer = ({data, chartSettings, chartInterval, alarmMarkersSettings}) => {  
+        
+        const mainChartContainerRef = useRef();
+        const chartRef = useInitializeChart(mainChartContainerRef);
+    
+        
+        const candlestickSeriesRef = useInitializeCandlestickSeries(chartRef);
+        const { ema10SeriesRef, ema55SeriesRef, ema200SeriesRef } = useInitializeEmasSeries(chartRef);
+    
+        //* Hooks
+        useSetCandlestickSeriesData(chartSettings.showCandlestickSerie, data, candlestickSeriesRef);
+        useSetEmasSeriesData(chartSettings.showEmasSerie, data, ema10SeriesRef, ema55SeriesRef, ema200SeriesRef);
+    
+        useSetupMarkers(candlestickSeriesRef, chartInterval, 
+            alarmMarkersSettings.showAlarmsMarkers, alarmMarkersSettings.showAlarmsSelectedMarkers, alarmMarkersSettings.showAlarmsFilteredMarkers,
+            chartSettings.showOrdersUsdmMarkers, chartSettings.showOrdersCoinmMarkers, chartSettings.showOrdersSpotMarkers, chartSettings.showOrdersStandardMarkers);
+        
+    
 
     return(
         <div
-            ref={chartContainerRef}            
+            ref={mainChartContainerRef}            
             className="h-full rounded-t-lg overflow-hidden"
       ></div>    
     )
--- strateger-react/src/components/Charts/MainChart/containers/ChartContainer.jsx
+++ strateger-react/src/components/Charts/MainChart/containers/ChartContainer.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+
 import { useSelector } from 'react-redux';
 
 import { selectTemporalidad, selectStartDate, selectCurrentDate } from 'reduxStore/toolBar';
@@ -6,15 +6,9 @@ import { selectTemporalidad, selectStartDate, selectCurrentDate } from 'reduxSto
 import StochasticChartContainer from './StochasticChartContainer';
 import CandlestickChartContainer from './CandlestickChartContainer';
 
-import useSetupMarkers from '../hooks/useSetupMarkers';
+
 import useFetchChartData from '../hooks/useFetchChartData';
 import useSetupChartParameters from '../hooks/useSetupChartParameters';
-import useInitializeChart from '../hooks/useInitializeChart';
-import useInitializeCandlestickSeries from '../hooks/useInitializeCandlestickSeries';
-import useInitializeEmasSeries from '../hooks/useInitializeEmasSeries';
-
-import useSetCandlestickSeriesData from '../hooks/useSetCandlestickSeriesData';
-import useSetEmasSeriesData from '../hooks/useSetEmasSeriesData';
 
 import LoadingOverlay from '../../../common/LoadingOverlay/LoadingOverlay';
 import ButtonsPanel from '../components/buttonsPanel/ButtonsPanel';
@@ -52,22 +46,6 @@ const ChartContainer = ({ showButtonsPanel, updateShowButtonsPanel }) => {
     const { chartStartDate, chartEndDate } = useSetupChartParameters(interval, startDate, endDate);
     const { data, loading, chartInterval } = useFetchChartData(chartStartDate, chartEndDate);
 
-    //!------------------------------ Main Chart ------------------------------!//
-    const mainChartContainerRef = useRef();
-    const chartRef = useInitializeChart(mainChartContainerRef);
-
-    //!------------------------------ Main Chart ------------------------------!//
-    const candlestickSeriesRef = useInitializeCandlestickSeries(chartRef);
-    const { ema10SeriesRef, ema55SeriesRef, ema200SeriesRef } = useInitializeEmasSeries(chartRef);
-
-    //* Hooks
-    useSetCandlestickSeriesData(chartSettings.showCandlestickSerie, data, candlestickSeriesRef);
-    useSetEmasSeriesData(chartSettings.showEmasSerie, data, ema10SeriesRef, ema55SeriesRef, ema200SeriesRef);
-
-    useSetupMarkers(candlestickSeriesRef, chartInterval, 
-        alarmMarkersSettings.showAlarmsMarkers, alarmMarkersSettings.showAlarmsSelectedMarkers, alarmMarkersSettings.showAlarmsFilteredMarkers,
-        chartSettings.showOrdersUsdmMarkers, chartSettings.showOrdersCoinmMarkers, chartSettings.showOrdersSpotMarkers, chartSettings.showOrdersStandardMarkers);
-    
 
     //!------------------------------ Render ------------------------------!//
     return (
@@ -85,7 +63,12 @@ const ChartContainer = ({ showButtonsPanel, updateShowButtonsPanel }) => {
 
             <div className="flex flex-col">
                 <div style={{ height: chartSettings.showStochasticSerie ? "400px" : "600px" }}>
-                    <CandlestickChartContainer chartContainerRef={mainChartContainerRef} />
+                    <CandlestickChartContainer
+                        data={data}
+                        chartSettings={chartSettings}
+                        chartInterval={chartInterval}
+                        alarmMarkersSettings={alarmMarkersSettings}
+                    />
                 </div>
                 <div style={{ height: chartSettings.showStochasticSerie ? "200px" : "0px" }}>
                     <StochasticChartContainer 
