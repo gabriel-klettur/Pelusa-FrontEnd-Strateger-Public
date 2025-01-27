9e14638a94505e456476deb8c1a6d2d7ab01f4f9
Gabriel Roca <blast.blackstone@gmail.com>
2025-01-27 14:07:34 +0000

    refactor(common/TabBar): helpers was removed and directly implemented in 'TabBar'

--- strateger-react/src/components/common/TabBar.jsx
+++ strateger-react/src/components/common/TabBar.jsx
@@ -1,31 +1,46 @@
-
 import { Tab, TabGroup, TabList } from '@headlessui/react';
-import { getTabClassName } from '../NavBar/utils/helpers';
 
-const NavBar = ({selectedTab, handleTabChange, tabs}) => {
-    return(
+const NavBar = ({ selectedTab, handleTabChange, tabs }) => {
+    return (
         <TabGroup vertical selectedIndex={selectedTab} onChange={handleTabChange}>
             <div className="flex">
-            <TabList className="w-12 h-auto mt-1">
-                {tabs.map((tab, index) => (
-                <Tab
-                    key={index}
-                    className={({ selected }) =>
-                        `w-full h-14 p-2 text-sm font-medium transition-colors duration-200 ${getTabClassName({ selected, disabled: tab.disabled })}`
-                    }
-                    disabled={tab.disabled}
-                >
-                    <img
-                        src={tab.icon}
-                        alt={tab.name}
-                        className={`h-8 w-8 mx-auto ${tab.disabled ? 'opacity-50' : ''}`}
-                    />
-                </Tab>
-                ))}
-            </TabList>
+                <TabList className="w-12 h-auto mt-1">
+                    {tabs.map((tab, index) => (
+                        <Tab
+                            key={index}
+                            className={({ selected }) => {
+                                const baseClasses =
+                                    'w-full h-14 p-2 text-sm font-medium transition-colors duration-200';
+                                const disabledClasses =
+                                    'cursor-not-allowed bg-gray-500 text-gray-500';
+                                const selectedClasses =
+                                    'bg-african_violet-500 text-african_violet-900';
+                                const unselectedClasses =
+                                    'bg-african_violet-300 text-african_violet-700 hover:bg-african_violet-300 hover:text-african_violet-900';
+
+                                return `${baseClasses} ${
+                                    tab.disabled
+                                        ? disabledClasses
+                                        : selected
+                                        ? selectedClasses
+                                        : unselectedClasses
+                                }`;
+                            }}
+                            disabled={tab.disabled}
+                        >
+                            <img
+                                src={tab.icon}
+                                alt={tab.name}
+                                className={`h-8 w-8 mx-auto ${
+                                    tab.disabled ? 'opacity-50' : ''
+                                }`}
+                            />
+                        </Tab>
+                    ))}
+                </TabList>
             </div>
         </TabGroup>
     );
-}
+};
 
 export default NavBar;
